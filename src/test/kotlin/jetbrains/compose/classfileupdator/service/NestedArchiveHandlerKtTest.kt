11fbaa8950a15063e3cf06634f55b777624fe02c
vikrant21st <30598062-vikrant21st@users.noreply.github.com>
2021-06-07 01:26:19 +0530

    Workflow updated for JUnit

---
+++ src/test/kotlin/jetbrains/compose/classfileupdator/service/NestedArchiveHandlerKtTest.kt
@@ -0,0 +1,16 @@
+package jetbrains.compose.classfileupdator.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NestedArchiveHandlerKtTest {
+
+    @Test
+    fun getCommand() {
+    }
+
+    @Test
+    fun getScript() {
+    }
+}
---
+++ src/test/kotlin/jetbrains/compose/classfileupdator/service/UploadFilesKtTest.kt
@@ -0,0 +1,12 @@
+package jetbrains.compose.classfileupdator.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class UploadFilesKtTest {
+
+    @Test
+    fun createAndCleanTempDirectory() {
+    }
+}
